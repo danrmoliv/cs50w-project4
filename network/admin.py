0d0c75ac7fb4669e6854f50dddae1f1e3b5cdb9a
Daniel Oliveira <67245632+danrmoliv@users.noreply.github.com>
2025-11-08 12:00:22 -0300

    Create PostItem and User models and register in admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, PostItem
 
 # Register your models here.
+admin.site.register(PostItem)
+admin.site.register(User)
--- network/models.py
+++ network/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+### TODO: Verificar como modelar following
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('self', related_name='following')
+    followers = models.ManyToManyField('self', related_name='followers') 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            'followers': [user.username for user in self.followers.all()],
+            'following': [user.username for user in self.following.all()]
+        }
+
+class PostItem(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(User, blank=True, related_name="posts_liked")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "creator": self.user.username,
+            "body": self.body,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "liked_by": self.liked_by
+        }
